44b503551cdfc86454a8ff964665fa2fd40c5de8
anantkpatel <imagine231216@gmail.com>
2026-07-11 09:29:08 +0530

    kl

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+// import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/checkout">Checkout</Link>
+
+    </div>
+    
+    </>
   )
 }
