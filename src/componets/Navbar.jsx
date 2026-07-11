471f9193395272ad8dcdbd7107bd1bf3968d2460
anantkpatel <imagine231216@gmail.com>
2026-07-11 18:40:31 +0530

    v

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import useCartStore from '../stores/CartStore'
 
 export default function Navbar() {
+  const cart = useCartStore((state)=>state.cart)
   return (
     <>
     <div className="nav bg-gray-500 flex justify-evenly items-center  ">
         <Link to="/">Home</Link>
         <Link to="/cart">Cart</Link>
         <Link to="/checkout">Checkout</Link>
+        <Link to ="/cart">Cart({cart.length}) </Link>
 
     </div>
     
