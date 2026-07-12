a00dd7d9342c9f6e54297a8a19252c50d82ddc8f
anantkpatel <imagine231216@gmail.com>
2026-07-12 11:49:58 +0530

    l

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,23 @@ import React from 'react'
 import useCartStore from '../stores/CartStore'
 
 export default function Cart() {
+  const cart = useCartStore((state)=>state.cart)
   return (
     <div className="max-w-6xl mx-auto p-8">
-    <div className="heading">
+    <div className="heading text-3xl font-bold mb-8">
       <h1>My Whishlist</h1>
     </div>
+   {cart.length===0?(
+    <h2>Your cart is empty</h2>
+   ):(
+     cart.map((item)=>{
+      <div key={item.id} className="key">
+        <h2>{item.title} </h2>
+        <p>Quantity:{item.quantity} </p>
+        <p>${item.price} </p>
+      </div>
+    })
+   )}
     </div>
   )
 }
